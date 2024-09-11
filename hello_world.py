65c351402e9e4c68feea19ba380a2cc7c4894a90
Pangmin2 <totoro7378@naver.com>
2024-09-11 15:37:53 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,36 @@
 def print_hello():
     print("Hello World!!")
 
+def print_kit():
+    print("눈에확띄게")
+def print_kit1():
+    print("눈에확띄게1")
+def print_kit2():
+    print("눈에확띄게2")
+def print_kit3():
+    print("눈에확띄게3")
+def print_kit4():
+    print("눈에확띄게4")
+def print_kit5():
+    print("눈에확띄게5")
+def print_kit6():
+    print("눈에확띄게6")
+def print_kit7():
+    print("눈에확띄게7")
+def print_kit8():
+    print("눈에확띄게8")
+def print_kit9():
+    print("눈에확띄게9")
+
 if __name__ == '__main__':
     print_hello()
+    print_kit()
+    print_kit1()
+    print_kit2()
+    print_kit3()
+    print_kit4()
+    print_kit5()
+    print_kit6()
+    print_kit7()
+    print_kit8()
+    print_kit9()
